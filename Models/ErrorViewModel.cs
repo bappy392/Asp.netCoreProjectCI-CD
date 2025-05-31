35f8aae8a4e865c836b1b55c9e89edbda2d7cce3
bappy392 <bappy392@gmail.com>
2025-05-31 13:00:53 +0600

    ASP log 1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeployGithubAsp.netCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
